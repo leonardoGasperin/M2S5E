127fd91ffe62e65bf55a31124c35deb46e45f6ea
LeonardoGasperin <gasperinlvg@gmail.com>
2022-10-25 19:42:29 -0300

    finished exercicie 2

---
+++ Enteties/Resources.cs
@@ -0,0 +1,9 @@
+﻿namespace M2S5E1.Enteties
+{
+    public enum Resources 
+    {
+        Mineral,
+        Aliments,
+        Materials
+    }
+}
---
+++ Enteties/StarPlanets.cs
@@ -0,0 +1,14 @@
+﻿namespace M2S5E1.Enteties
+{
+    internal class StarPlanets : CelestialStars
+    {
+        private Dictionary<Resources, int> resources = new Dictionary<Resources, int>();
+        private bool isHabitable = false;
+        public StarPlanets(string name, double size, double mass, double gravity, int approximateAge, 
+                           Dictionary<Resources, int> resources) : base(name, size, mass, gravity, approximateAge)
+        {
+            this.resources = resources;
+        }
+    }
+}
+
