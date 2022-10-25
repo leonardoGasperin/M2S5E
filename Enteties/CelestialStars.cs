dcb17bf5f3290284735dbca01562fcc7cc4d74c7
LeonardoGasperin <gasperinlvg@gmail.com>
2022-10-25 19:23:49 -0300

    completed Exercicie 1

---
+++ Enteties/CelestialStars.cs
@@ -0,0 +1,20 @@
+﻿namespace M2S5E1.Enteties
+{
+	public class CelestialStars
+	{
+		protected string name;
+		protected double size;
+		protected double mass;
+		protected double gravity;
+		protected int approximateAge = 0;
+
+		public CelestialStars(string name, double size, double mass, double gravity, int approximateAge)
+		{
+			this.name = name;
+			this.size = size;
+			this.mass = mass;
+			this.gravity = gravity;
+			this.approximateAge = approximateAge;
+		}
+	}
+}
